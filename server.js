4bb792db1717c44ef5ad5a9cfb719bdd5ff35d67
flybaflyba <52888193+flybaflyba@users.noreply.github.com>
2020-05-13 18:02:41 -1000

    Configure server.js to connect to database (with info in database.config.js). Starting ine about 8 in server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var jwt = require('jsonwebtoken');//用来创建和确认用户信息摘要
 
+
 //一些配置
 // Configuring the database
 const dbConfig = require('./config/database.config.js');
@@ -21,20 +22,28 @@ mongoose.connect(dbConfig.database, {
     console.log('Could not connect to the database. Exiting now...', err);
     process.exit();
 });
+
+
+
 app.set('superSecret', dbConfig.secret); // 设置app 的超级密码--用来生成摘要的密码
 //用body parser 来解析post和url信息中的参数
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 // 使用 morgan 将请求日志打印到控制台
 app.use(morgan('dev'));
+
 //路由
 //基础路由
 app.get('/',function(req,res){
     res.send("Hello from Litian");
 })
+
+//
 //for product: 
-var setupRoute = require('./app/routes/product.route');// 导入路由文件
-app.use('/',setupRoute);   //设置访问路径
+//var setupRoute = require('./app/routes/product.route');// 导入路由文件
+//app.use('/',setupRoute);   //设置访问路径
+//
+
 var port = process.env.PORT || 8080; // 设置启动端口
 // 启动服务
 app.listen(port);
