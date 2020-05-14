36ad27f62b4152809d141b81aad615643d7128f6
flybaflyba <52888193+flybaflyba@users.noreply.github.com>
2020-05-13 18:17:32 -1000

    Edit product.route.js. Add PATCH action, Now user can use PATCH with id number to update a product.

--- app/routes/product.route.js
+++ app/routes/product.route.js
@@ -92,6 +92,63 @@ router.get('/products/:productId',function(req,res){
         
     })
 
+
+
+// 更新
+router.patch('/products/:productId',function(req,res){
+        var myDate = new Date();
+        var date=myDate.toLocaleDateString( );// 获取日期与时间
+        var product_id= req.body.product_id;
+        var product_name= req.body.product_name;
+        var price= req.body.price;
+        var description= req.body.description;
+        var image= req.body.image;
+        var link= req.body.link;
+        //var created_date= req.body.created_date;
+        var updated_date= date;
+        var user_id= req.body.user_id;
+        var list_id= req.body.list_id;
+        
+        Product.findByIdAndUpdate(req.params.productId,
+            {product_id:product_id,
+            product_name:product_name,
+            price:price,
+            description:description,
+            image:image,
+            link:link,
+            //created_date:created_date,
+            updated_date: updated_date,
+            user_id:user_id,
+            list_id:list_id        
+            },{new: true})
+                .then(product => {
+                    if(!product) {
+                        return res.status(404).send({
+                            message: "Product not found with id " + req.params.productId
+                        });            
+                    }
+                    
+                
+                    res.send({message: "Product updated"});
+                }).catch(err => {
+                    if(err.kind === 'ObjectId') {
+                        return res.status(404).send({
+                            message: "Product not found with id " + req.params.productId
+                        });                
+                    }
+                
+                });
+                /*
+            function(err,product){
+            if(err){
+                res.json({
+                    success:false,message:"更新失败"
+                })
+            }
+        })
+        res.json({success:true,message:"更新成功"})
+        */
+    })
     
 
 module.exports = router;
