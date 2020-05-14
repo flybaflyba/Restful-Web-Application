8bc33cd0f2302a60b5322cf0122c43bd563492f6
flybaflyba <52888193+flybaflyba@users.noreply.github.com>
2020-05-13 17:58:03 -1000

    Re-construct folders and files locations

--- config/database,config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-        'secret':'mynameislitianzhang',    
-        'database':'mongodb://127.0.0.1'
-    }
---
+++ config/database.config.js
@@ -0,0 +1,7 @@
+
+
+module.exports = {
+    'secret':'mynameislitianzhang',    
+    'database':'mongodb://127.0.0.1'
+}
+
