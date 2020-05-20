388a10c8c0c52e5165057f281f7e4a801ca8ceb2
flybaflyba <52888193+flybaflyba@users.noreply.github.com>
2020-05-19 23:41:26 -1000

    Edit database.config.js, change the database link in there so that server can connect to database in container, link is different from the link we used when mongo is running on laptop, now it's in container

--- config/database.config.js
+++ config/database.config.js
@@ -2,6 +2,11 @@
 
 module.exports = {
     'secret':'mynameislitianzhang',    
-    'database':'mongodb://127.0.0.1'
+    //'database':'mongodb://127.0.0.1'
+    'database':'mongodb://mongo:27017'
+
+
+    
 }
+
 
