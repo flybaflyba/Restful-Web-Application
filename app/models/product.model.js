216a0169562f9b6bec366e99b4b0eaf2263c19c5
flybaflyba <52888193+flybaflyba@users.noreply.github.com>
2020-06-02 00:15:45 -1000

    Update product.model.js file, make some fields required and unique, such as email address. (final version of product.model.js is here now)

--- app/models/product.model.js
+++ app/models/product.model.js
@@ -1,16 +1,24 @@
 const mongoose = require('mongoose');
 const ProductSchema = mongoose.Schema({
  
-    product_id: String, 
-    product_name: String, 
+    //product_id: String, 
+    product_name: {
+        type: String,
+        required: true
+    }, 
     price: String, 
     description: String, 
     image: String, 
     link: String, 
     created_date: String, 
     updated_date: String, 
-    user_id: String, 
+    //user_id: String, 
     list_id: String,
+
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
     
 });
 module.exports = mongoose.model('Product', ProductSchema);
