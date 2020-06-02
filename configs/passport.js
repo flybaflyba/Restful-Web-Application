7589a8e42a085904d79f4acc1e468373d1e16527
flybaflyba <52888193+flybaflyba@users.noreply.github.com>
2020-06-02 00:26:31 -1000

    Edit passport.js

--- configs/passport.js
+++ configs/passport.js
@@ -3,13 +3,16 @@ const ExtractJwt = require("passport-jwt").ExtractJwt
 
 const User =  require('../app/models/user.model')
 
+//this is a secret we use to sign the password 
 const config = require('./config')
 const secret = config.secret
+
 
 module.exports = (passport) => {
     var opts = {}
     opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme('jwt')
     opts.secretOrKey = secret
+    //after user is signed in, we will know who that they and if someone is a user of ours 
     passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
         User.findOne({_id: jwt_payload._id}, (err, user) => {
             if (err) {
