8b8f73eee5fc6dd142e648f741f0eb31304effbd
flybaflyba <52888193+flybaflyba@users.noreply.github.com>
2020-06-15 17:19:47 -1000

    Create store.js

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,3 +1,8 @@
+
+
+//Create store file, so that we can use things in this file anywhere else
+//
+
 import Vue from "vue";
 import Vuex from "vuex";
 import AuthService from "@/services/auth";
@@ -11,9 +16,11 @@ const initialState =
 
 Vue.use(Vuex)
 
+//we store our token in this file so that we know if we are signed in ou not 
 export const store = new Vuex.Store({
          state: initialState,
          actions: {
+           //this is a method we can call in out login vue, so taht we save token into this file while login 
            login({ commit }, user) {
              return AuthService.login(user)
                .then((token) => {
@@ -45,6 +52,7 @@ export const store = new Vuex.Store({
              state.token = null;
            },
          },
+         //getters we returns these info, so that we can use them in other parts of the app
          getters: {
            loggedIn: (state) => state.loggedIn,
            token: (state) => state.token,
