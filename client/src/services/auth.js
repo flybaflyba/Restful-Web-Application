a2b475adbd72207af0b21008af2c0f659019da7b
flybaflyba <52888193+flybaflyba@users.noreply.github.com>
2020-06-15 15:00:04 -1000

    Create login function

--- client/src/services/auth.js
+++ client/src/services/auth.js
@@ -3,37 +3,60 @@ import axios from 'axios';
 const API_URL = "http://localhost:3000/api/auth";
 
 class AuthService {
-    static signup(user) {   
-        //console.log(user.email);
-        return new Promise((resolve, reject) => {
-            //console.log(user.email);
-            axios
-              .post(API_URL + "/signup", {
-                  user_name: user.user_name,
-                  email: user.email,
-                  password: user.password,
-                  last_name: user.last_name,
-                  first_name: user.first_name,
-                
-              })
-              .then((res) => {
-                console.log("Service return success");
-                //resolve(res.data.user);
-                resolve(res.data.user);
-                  
-                //console.log(user.email);
-                //console.log(user.password);
-              })
-              .catch((err) => {
-                console.log("Service return failure");
-                reject(err);
+  static signup(user) {
+    //console.log(user.email);
+    return new Promise((resolve, reject) => {
+      //console.log(user.email);
+      axios
+        .post(API_URL + "/signup", {
+          user_name: user.user_name,
+          email: user.email,
+          password: user.password,
+          last_name: user.last_name,
+          first_name: user.first_name,
+        })
+        .then((res) => {
+          console.log("Service return success");
+          //resolve(res.data.user);
+          resolve(res.data.user);
 
-                //user.email = "heyauth"
-                //console.log(user.email);
-                //console.log(user.password);
-              });
+          //console.log(user.email);
+          //console.log(user.password);
+        })
+        .catch((err) => {
+          console.log("Service return failure");
+          reject(err);
+
+          //user.email = "heyauth"
+          //console.log(user.email);
+          //console.log(user.password);
         });
+    });
+  }
+
+  static login(user) {
+    return new Promise((resolve, reject) => {
+      axios
+        .post(API_URL + "/login", {
+          email: user.email,
+          password: user.password,
+
+        })
+        .then((res) => {
+          console.log("login success");
+          resolve(res.data.token);
+        })
+        .catch((err) => {
+          console.log("login failed");
+            reject(err);
+            console.log(user.email);
+            console.log(user.password);
+            
+        });
+    });
     }
+    
+
 }
  
 export default AuthService;
--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,5 +1,83 @@
 <template>
   <div class="login">
     <h1>Login</h1>
+
+<form class="loginForm" @submit.prevent="handleLogin">
+
+     <div v-if="message" id="message">{{ message }}</div>
+
+        <div class="form_row">
+            <label for="email">Email: </label>
+            <input type="email" name="email" v-model="email" />
+        </div>
+
+        <div class="form_row">
+            <label for="password">Password: </label>
+            <input type="password" name="password" v-model="password" />
+        </div>
+        
+        <p>Required fields are marked with *</p>
+        <div class="form_row">
+            <button :disabled="submitted">
+                <span>Login</span>
+            </button>
+        </div>
+    </form>
+
+
   </div>
 </template>
+
+
+
+<script>
+
+import AuthServive from "@/services/auth"
+
+export default {
+    name: "Login",
+    data() {
+        return {
+        submitted: false,
+        message: "",
+        email: "",
+        password: "",
+    }
+    },
+    methods: {
+        handleLogin() {
+            console.log("Sign Up Pressed");
+            this.submitted = true;
+
+            var user = {
+                email: this.email,
+                password: this.password,
+            }
+
+            if (this.email != "" && this.password != "" && this.user_name != "") {
+                //this.message = "I should send data to API";
+                console.log("Sending login request");
+
+                AuthServive.login(
+                    user
+                )
+                    .then((token) => {
+                        console.log(token);
+                        this.message = "Logged in"
+                    }) 
+                    .catch(err => {
+                        console.log(err);
+                        this.message = "Invalid Email/Password";
+                        this.submitted = false;
+                    });
+
+            } else {
+                this.message = "Email or password missing";
+                this.submitted = false;
+            }
+
+            
+        }
+    }
+}
+</script>
