ed0b3e73e5bad1ea73a1a499f5402dd52fc0aa4f
flybaflyba <52888193+flybaflyba@users.noreply.github.com>
2020-06-15 17:33:01 -1000

    Created product service, so that we can get products from out database through api

--- client/src/services/product.js
+++ client/src/services/product.js
@@ -1,13 +1,17 @@
+
+
+//this file communicates with our api 
+
 import axios from 'axios';
 
 const API_URL = "http://localhost:3000/api/products";
 
 class ProductService {
     static getProducts(token) {
-        console.log("token is: " + token);
+        console.log("token is: " + token); //debug...whats the token 
         return new Promise((resolve, reject) => {
             axios
-                .get(API_URL, { headers: { authorization: token } })
+                .get(API_URL, { headers: { authorization: token } }) //pass in the token in the header when we request 
                 .then((res) => {
                     console.log("Service return success");
                     resolve(res.data);
