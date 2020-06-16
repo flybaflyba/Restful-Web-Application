36275a83eed9321b4b251880d0b45b7a90992562
flybaflyba <52888193+flybaflyba@users.noreply.github.com>
2020-06-16 12:19:21 -1000

    Update Home and About pages

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="about">
     <h1>About</h1>
+
+    <h2>We Share What We Like Here</h2>
+
+
+    <img class="img" 
+    src= "http://tinobusiness.com/wp-content/uploads/2015/09/CONSUMER-PRODUCTS-e1442909155136.jpg"
+    alt="Image not load" >
+
   </div>
 </template>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -3,6 +3,15 @@
 <template>
   <div class="home">
     <h1>Home</h1>
+    <h2>Welcome to Litian's CS490R Project: 《Product Review》in BYUH</h2>
+
+
+    <img class="img" 
+    src= "https://brightspotcdn.byu.edu/dims4/default/58d7c60/2147483647/strip/true/crop/1361x776+0+15/resize/930x530!/quality/90/?url=https%3A%2F%2Fbrigham-young-brightspot.s3.amazonaws.com%2F60%2F37%2F734094314b46a54e776685dabca2%2F5199133281-66f9bfff87-o.jpg" 
+    alt="Image not load" >
+
+
+
   </div>
 </template>
 
@@ -18,3 +27,11 @@
 //   }
 // };
 </script>
+
+<style scoped>
+
+h2 {
+  padding: 30px;
+}
+
+</style>
